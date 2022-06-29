1cf1806b0160e35992f56ec37afb7ad448991606
DanielMurillo24 <62394689+DanielMurillo24@users.noreply.github.com>
2022-06-29 16:00:00 -0600

    Update streamlit_app.py
    Added the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket Smootthie')
+streamlit.text('Hard-boile Free-Range Egg')
+
