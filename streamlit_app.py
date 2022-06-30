17a646a042afd5753bd7c368defa5a51640db83b
DanielMurillo24 <62394689+DanielMurillo24@users.noreply.github.com>
2022-06-30 14:36:03 -0600

    Update streamlit_app.py
    Add emojis to the description

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,9 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & Rocket Smootthie')
-streamlit.text('Hard-boile Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, spinach & Rocket Smootthie')
+streamlit.text('🐔 Hard-boile Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toats')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
